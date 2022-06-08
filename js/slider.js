019995d6af8a988ea4afc6221513ec0df686b4e6
Joranvnp <joran.vanpeene@gmail.com>
2022-06-08 08:51:45 +0200

    modification slider carousel

--- js/slider.js
+++ js/slider.js
@@ -11,9 +11,14 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
+  let slideIndex = 1;
+  //Selectionne l'élément pour le slide avec la class mySlides
   let slides = document.getElementsByClassName("mySlides");
+  //Selectionne l'élément pour le dot avec la class mySlides
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
+  //Remet à 1 l'index si il est supèrieur au nombre de slides
+  if (n > slides.length) {slideIndex = 1}
+  //Remet à 1 l'index si il est infèrieur au nombre de slides
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
@@ -21,22 +26,12 @@ function showSlides(n) {
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
+    // slides[i].className = dots[i].className.replace
     // slides[i].querySelectorAll = document.querySelectorAll("masthead.header");
     // dots[i].className = dots[i].className.replace.style.backgroundImage = "url(./assets/img/map-image.png)";
     console.log("remplace l'ancienne image");
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  console.log("")
+  // console.log("")
 }
-
-// var images = ['./assets/img/header-bg.jpg'];
-// var index = 0;
-// function buildImage() {
-//     var img = document.createElement('img');
-//     img.src = images[index];
-//     document.getElementById('content').style.backgroundImage = (img);
-//     index++;
-//     index = index%4;
-//     img.src = images[index];
-// }
